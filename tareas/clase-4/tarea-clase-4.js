72661894bf554f9b1d1c7ba0a5229b05c749f901
Ivan Kaczek <ivan.kaczek@gmail.com>
2022-09-28 19:36:08 -0300

    subo tarea clase 4

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -1,9 +1,22 @@
 // TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
 
-
+function tarea1() {
+  console.log("Tarea 1");
+  for (let i = 3; i <= 22; i += 3) {
+    console.log(i);
+  }
+}
 // TAREA: Usando un bucle 'while', decile a tu computadora que registre los números de
 //       diez a uno.
 
+function tarea2() {
+  console.log('Tarea 2');
+  let contadorBucle = 10;
+  while (contadorBucle > 0) {
+    console.log(contadorBucle);
+    contadorBucle--;
+  }
+}
 
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
 // Cuenta del 1 al 50 e imprime los números:
@@ -17,5 +30,62 @@
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
 
+function tarea3resolucionIvan() {
+    console.log('Ejercicio FizzBuzz');
+    for (let i = 1; i <= 50; i++) {
+        if(i%15 === 0) {
+            console.log('FizzBuzz');
+        } else if (i%5=== 0){
+            console.log('Buzz');
+        } else if (i%3===0){
+            console.log('Fizz');
+        } else {
+            console.log(i);
+        }
+    }
+
+}
+
+function FizzBuzzResolucionElegante(){
+    for (let i = 1; i < 50; i++) {
+      let texto = "";
+      if(i%3 === 0){
+        texto='Fizz';
+      }
+      if(i%5=== 0){
+        texto += 'Buzz';
+      }
+      console.log (texto || i);
+    }
+}
+
+
+
 // Calcular el promedio de todos los números en un array de números. (y ponerlo en una función)
 // ej.: calcularPromedio([10,5,4,2,8])
+
+function calcularPromedio(arrayEnteros){
+    let acumulado = 0;
+    for (let i = 0; i < arrayEnteros.length; i++) {
+        acumulado += arrayEnteros[i];
+    }
+    return acumulado/arrayEnteros.length;
+}
+
+function tarea4() {
+    console.log('Calculo de promedios');
+    const arrayDeEnteros = [4,5,10,22,15,24,12];
+    const promedio = calcularPromedio(arrayDeEnteros);
+    console.log(`promedio = ${promedio}`);
+}
+
+
+
+function ejecutarTareasClase4() {
+  tarea1();
+  tarea2();
+  tarea3resolucionIvan();
+  tarea4();
+}
+
+ejecutarTareasClase4();
