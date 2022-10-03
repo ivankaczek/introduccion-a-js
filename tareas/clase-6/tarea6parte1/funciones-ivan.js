ef081aa4fa990fc7b53a224226c80ec1496a6f92
Ivan Kaczek <ivan.kaczek@gmail.com>
2022-10-03 12:45:06 -0300

    subo tarea clase 6 parte 1

--- tareas/clase-6/tarea6parte1/funciones-ivan.js
+++ tareas/clase-6/tarea6parte1/funciones-ivan.js
@@ -1,3 +1,66 @@
-function alertarEsto(texto){
-    alert(texto);
+function preguntarCantidadPersonas(){
+    const pregunta = 'Cuánta gente hay en el grupo familiar?';
+    const cantidad = Number(prompt(pregunta));
+    return cantidad;
 }
+/*
+function traerElementoConEsteId(id){
+    const parametro = `'#${id}'`
+    return document.querySelector(parametro);
+}
+*/
+
+function traerTitulo(){
+    return document.querySelector('#titulo');
+}
+
+function crearLabel(){
+    const $label = document.createElement('label');
+    const textoLabel = 'Ingrese la edad de la persona: ';
+    $label.textContent = textoLabel;
+    return $label;
+}
+
+function crearInput(){
+    const $input = document.createElement('input');
+    $input.placeholder = "edad";
+    $input.className = "datos";
+    return $input;
+}
+
+function agregarLabelInput(){
+    const $elementoDiv = document.createElement('div');
+    const $labelParaAgregar = crearLabel();
+    const $inputParaAgregar = crearInput();
+    $elementoDiv.appendChild($labelParaAgregar);
+    $elementoDiv.appendChild($inputParaAgregar);
+    return $elementoDiv;
+}
+
+function promedio(numeros){
+    let acumulado = 0;
+    for(let i = 0; i<numeros.length ; i++){
+      acumulado += numeros[i];
+    }
+    return (acumulado/numeros.length).toFixed(2);
+  }
+
+  function devolverMaximo(numeros){
+    let resultado = numeros[0];
+    for (let i = 0; i<numeros.length ; i++) {
+      if(numeros[i] > resultado){
+        resultado = numeros[i];
+      }
+    }
+    return resultado;
+}
+
+function devolverMinimo(numeros){
+    let resultado = numeros[0];
+    for (let i = 0; i<numeros.length ; i++) {
+      if(numeros[i] < resultado){
+        resultado = numeros[i];
+      }
+    }
+    return resultado;
+  }
--- tareas/clase-6/tarea6parte1/tarea6parte1.js
+++ tareas/clase-6/tarea6parte1/tarea6parte1.js
@@ -1,11 +1,62 @@
-alert('hi');
-alertarEsto('hols');
+
 
 /*
 TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
 Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad 
+y el promedio del grupo familiar.
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs 
 ya creados (investigar cómo en MDN).
 */
+
+// variables con las que voy a trabajar
+const cantidadIntegrantes = preguntarCantidadPersonas();
+let edades = [];
+
+// elementos HTML con los que voy a trabajar
+
+const $tituloPrincipal = traerTitulo();
+const $elementoParaMostrarInputs = document.querySelector('#mostrar-inputs');
+const $botonCalcular = document.querySelector('#calcular');
+const $elementoParaMostrarResultados = document.querySelector('#mostrar-resultados');
+const $elementoMenorEdad = document.querySelector('#menor-edad');
+const $elementoMayorEdad = document.querySelector('#mayor-edad');
+const $elementoPromedioEdad = document.querySelector('#promedio-edad');
+
+// cambios a los elementos html
+$tituloPrincipal.textContent = 'calculos con las edades de las personas';
+
+// agrego en ese espacio que creé para mostrar inputs tantos labels+inputs como personas hay
+for(let i=0; i < cantidadIntegrantes; i++){
+    let $casilleroParaRellenar = agregarLabelInput();
+    $elementoParaMostrarInputs.appendChild($casilleroParaRellenar);
+}
+
+// quiero agregar al array de las edades el value de cada input
+
+$botonCalcular.onclick = function() {
+    const inputs = document.querySelectorAll('.datos');
+    console.log(inputs);
+    for(let i= 0; i<inputs.length; i++){
+        const valorParaAgregar = Number(inputs[i].value);
+        edades.push(valorParaAgregar);
+    }
+    console.log(edades);
+    $textoMenorEdad = document.createTextNode(`${devolverMinimo(edades)}`);
+    $elementoMenorEdad.appendChild($textoMenorEdad);
+
+    $textoMayorEdad = document.createTextNode(`${devolverMaximo(edades)}`);
+    $elementoMayorEdad.appendChild($textoMayorEdad);
+
+    $textoPromedioEdad = document.createTextNode(`${promedio(edades)}`);
+    $elementoPromedioEdad.appendChild($textoPromedioEdad);
+
+    $elementoParaMostrarResultados.className = "";
+    
+    return false;
+}
+
+
+
+
